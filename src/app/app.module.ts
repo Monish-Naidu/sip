41eb167f57f255aa86963f615be9ed575c7c3cc9
Brian <lam.b@husky.neu.edu>
2019-04-10 18:52:46 -0700

    created some stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/user/login/login.component';
+import { RegisterComponent } from './views/user/register/register.component';
+import { ProfileComponent } from './views/user/profile/profile.component';
+import { LocationComponent } from './views/website/location/location.component';
+import { ControlComponent } from './views/admin/control/control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    LocationComponent,
+    ControlComponent
   ],
   imports: [
     BrowserModule,
