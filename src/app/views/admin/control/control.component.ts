1436f33644cad57eaef7d2e0702fc198f5f4fa35
Brian <lam.b@husky.neu.edu>
2019-04-12 18:52:04 -0700

    finished with some stuff

--- src/app/views/admin/control/control.component.ts
+++ src/app/views/admin/control/control.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-control',
   templateUrl: './control.component.html',
-  styleUrls: ['./control.component.css']
+  styleUrls: ['../../../../../node_modules/bootstrap/dist/css/bootstrap.css', './control.component.css']
 })
 export class ControlComponent implements OnInit {
+  @ViewChild('f') controlForm: NgForm;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  deleteUser() {
+
   }
 
 }
